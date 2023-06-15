adb10cd08dd72a449c9f259e3d95b7b2c643aba4
Jongbin <jbLee@bluedigm.com>
2023-06-15 10:55:41 +0900

    환경변수 관련 설정변경

--- src/server.js
+++ src/server.js
@@ -2,7 +2,6 @@ import app from "./app.js";
 import http from 'http';
 import startHttpServer from './httpServer.js'
 import startWebSocketServer from './webSocketServer.js'
-import dotenv from 'dotenv'
 
 const PORT = process.env.NODE_PORT||80;
 
